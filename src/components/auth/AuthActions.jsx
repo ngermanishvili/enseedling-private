4f4e30a21e17d478eba0c0ecdfc626b8d545d22c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-07-30 14:39:19 +0400

    add components folder and destructure auth jwt functionally

---
+++ src/components/auth/AuthActions.jsx
@@ -0,0 +1,22 @@
+// AuthActions.js
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+const AuthActions = () => {
+  const navigate = useNavigate();
+  const [, , removeCookie] = useCookies();
+
+  const logOut = () => {
+    removeCookie("jwt");
+    navigate("/login");
+  };
+
+  return (
+    <div className="auth-actions">
+      <button onClick={logOut}>Log out</button>
+    </div>
+  );
+};
+
+export default AuthActions;
